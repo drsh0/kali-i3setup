5877de5d42028e01d685ed4276e70e5bf86ee284
drsh0 <29874997+drsh0@users.noreply.github.com>
2020-08-07 16:45:30 +1000

    synced config and files

--- i3setup/sync-folder.sh
+++ i3setup/sync-folder.sh
@@ -4,4 +4,7 @@ cp -r -u -v ~/i3setup ~/Documents/kali-i3setup/
 
 echo "syncing with git"
 git -C "/home/drsh0/Documents/kali-i3setup/" add -A --verbose
+git -C "/home/drsh0/Documents/kali-i3setup/" commit -m "synced config and files"
+git -C "/home/drsh0/Documents/kali-i3setup/" push origin
 
+echo "syncing complete"
