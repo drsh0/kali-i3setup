7c1b67e68fc564f6aed48ad4bcc90b5aabfabcba
drsh0 <29874997+drsh0@users.noreply.github.com>
2021-02-14 11:52:02 +1100

    add xfce 4.16 i3workspace notes

--- i3setup/packages.sh
+++ i3setup/packages.sh
@@ -17,7 +17,9 @@ sudo apt install adapta-gtk-theme papirus-icon-theme -y
 sudo apt install xfce4-genmon-plugin xfce4-xkb-plugin xfce4-fsguard-plugin xfce4-goodies -y
 sudo apt install j4-dmenu-desktop
 sudo apt install gtk3-nocsd
-git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
+sudo apt install libxfce4ui-2-dev
+sudo apt install libxfce4panel-2.0-dev
+#git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
 
 #security
 sudo apt install seclists gobuster -y
